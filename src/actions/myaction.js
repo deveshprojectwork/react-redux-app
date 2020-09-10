305e77a9e348f04f86bfeb5f7f00a44b2fbddda1
devesh <devesh.kr.srivastava@gmail.com>
2020-09-10 08:55:25 +0530

    adding async - await in myaction.js

--- src/actions/myaction.js
+++ src/actions/myaction.js
@@ -6,11 +6,18 @@
 // })
 
 export const anotherName = () =>{
-    return(dispatch)=>{
-       fetch("https://jsonplaceholder.typicode.com/users")
-       .then(res=>res.json())
-       .then(res2=>{
-           dispatch({type:"CHANGE_NAME", payload:res2[0].name})
-       })
-    }
+    // return  (dispatch)=>{
+    //    fetch("https://jsonplaceholder.typicode.com/users")
+    //    .then(res=>res.json())
+    //    .then(res2=>{
+    //        dispatch({type:"CHANGE_NAME", payload:res2[0].name})
+    //    })
+    // }
+
+    //with asnc and await : this is like promise
+    return async  (dispatch)=>{
+        const data = await fetch("https://jsonplaceholder.typicode.com/users")
+        const res2 = await data.json()
+            dispatch({type:"CHANGE_NAME", payload:res2[0].name})
+     }
 }
